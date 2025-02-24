115dda9050452761c1ec4f465c4b003927c8d0be
COSMOF0X <brosh1566@gmail.com>
2025-02-23 21:14:44 -0700

    Added additional functions for data analysis.
    After successfully making a function for analyzing the average stellar surface density between spiral and elliptical galaxies, I added additional capabilities for better understanding of the result. I made a function to analyze  the stellar mass and radius compared to the galaxy type to see how much weight the two factors had on determining stellar surface density. I additionally graphed the stellar surface density of the two galaxy types over the redshift to see how age of galaxies determine the stellar surface density. I additionally printed some values to get exact numbers and calculated the p-value using t-tests for most of the functions. I plan to add a correlational test to the stellar surface density and redshift graph and may do further analysis on it.

--- SurfaceStellarDensityandGalaxyTypesResearch/dataAnalysis.py
+++ SurfaceStellarDensityandGalaxyTypesResearch/dataAnalysis.py
@@ -1,5 +1,6 @@
-import pandas as pd
 import math
+from scipy import stats
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -30,7 +31,9 @@ ellipticalMedStelMass = np.power(10, ellipticalMedStelMassRaw)
 ellipticalDeVanRadRBand = ellipticalGalDataCSV['DeVancoulerGalRad_bandR_arcsec'].to_numpy()
 
 # Create galaxy morphology type array:
-galMorphTypeNames = ["Spiral\n(Sample Size: 10,000 Galaxies)", "Elliptical\n(Sample Size: 10,000 Galaxies)"]
+galMorphTypeNames1 = np.array(["Spiral\n(Sample Size: 10,000 Galaxies)", "Elliptical\n(Sample Size: 10,000 Galaxies)"])
+galMorphTypeNames2 = np.array(["Spiral (Sample Size: 10,000 Galaxies)", "Elliptical (Sample Size: 10,000 Galaxies)"])
+
 
 # Functions for data analysis:
 def stelSurfDen(stelMass, rad):
@@ -41,7 +44,12 @@ def stdError(data):
     # SE = σ / √(N)
     return ((np.std(data, ddof = 1)) / ((data.size)**0.5))
 
-def analyzeStelSurfDenandGalType(galTypeNames, stelMassSpiral, radSpiral, stelMassElliptical, radElliptical):
+def tTest(groups):
+    tStat, pVal = stats.ttest_ind(groups[0], groups[1])
+
+    return np.array([tStat, pVal])
+
+def analyzeAvgStelSurfDenandGalType(galTypeNames, stelMassSpiral, radSpiral, stelMassElliptical, radElliptical):
     # Spiral:
     stelSurfDenDataSpiral = stelSurfDen(stelMassSpiral, radSpiral)
     stelSurfDenAvgSpiral = np.mean(stelSurfDenDataSpiral)
@@ -53,28 +61,157 @@ def analyzeStelSurfDenandGalType(galTypeNames, stelMassSpiral, radSpiral, stelMa
     stelSurfDenErrorElliptical = stdError(stelSurfDenDataElliptical)
 
     # Group spiral and elliptical:
-    avgStelSurfDens = [stelSurfDenAvgSpiral, stelSurfDenAvgElliptical]
-    stelSurfDenErrors = [stelSurfDenErrorSpiral, stelSurfDenErrorElliptical]
+    stelSurfDenDataGrouped = np.array([stelSurfDenDataSpiral, stelSurfDenDataElliptical])
+    avgStelSurfDens = np.array([stelSurfDenAvgSpiral, stelSurfDenAvgElliptical])
+    stelSurfDenErrors = np.array([stelSurfDenErrorSpiral, stelSurfDenErrorElliptical])
+
+    # T-Test:
+    tResult = tTest(stelSurfDenDataGrouped)
 
     # Set up display parameters:
-    barGraphColors = ['blue', 'orange']
-    errorParams = {'ecolor': 'red', 'elinewidth': 2, 'capsize': 20, 'capthick': 2}
+    barGraphColors = ['cyan', 'orange']
+    errorParams = {'ecolor': 'red', 'elinewidth': 2, 'capsize': 15, 'capthick': 2}
 
-    # Display Data
+    # Display Data:
+    plt.clf()
     plt.figure()
     plt.bar(galTypeNames, avgStelSurfDens, yerr = stelSurfDenErrors, color = barGraphColors, edgecolor = 'black', linewidth = 2, error_kw = errorParams)
 
     plt.yscale('log')
-    plt.xlabel("Galaxy Morphological Type")
-    plt.ylabel("Average Stellar Surface Density (M☉/(arcsecond^2))")
-    plt.title("Galaxy Average Stellar Surface Density and Morphological Type\n(Derived from SDSS DR18)")
+    plt.xlabel("Galaxy Morphological Type", fontweight = 'bold')
+    plt.ylabel("Average Stellar Surface Density (M☉/(arcsecond^2))", fontweight = 'bold')
+    plt.title("Galaxy Average Stellar Surface Density and Morphological Type\n(Derived from SDSS DR18)", fontweight = 'bold')
 
     plt.tight_layout()
-    plt.savefig("Graphs/avgStelSurfDenandGalMorphType.png")
+    plt.savefig("Graphs/avgStelSurfDenandGalMorphType.png", bbox_inches= 'tight')
 
-    plt.show()
+    print(f"Average Stellar Surface Density (Spiral): {avgStelSurfDens[0]:.10}")
+    print(f"Average Stellar Surface Density (Elliptical): {avgStelSurfDens[1]:.10}")
+    print(f"T-Statistic: {tResult[0]:.10e}")
+    print(f"P-Value: {tResult[1]:.10e}")
+
+    return
+
+def analyzeAvgMedStelMassandGalType(galTypeNames, stelMassSpiral, stelMassElliptical):
+    # Spiral:
+    stelMassAvgSpiral = np.mean(stelMassSpiral)
+    stelMassErrorSpiral = stdError(stelMassSpiral)
+
+    # Elliptical:
+    stelMassAvgElliptical = np.mean(stelMassElliptical)
+    stelMassErrorElliptical = stdError(stelMassElliptical)
+
+    # Group spiral and elliptical:
+    stelMassDataGrouped = np.array([stelMassSpiral, stelMassElliptical])
+    avgStelMasses = np.array([stelMassAvgSpiral, stelMassAvgElliptical])
+    stelMassErrors = np.array([stelMassErrorSpiral, stelMassErrorElliptical])
+
+    # T-Test:
+    tResult = tTest(stelMassDataGrouped)
+
+    # Set up display parameters:
+    barGraphColors = ['cyan', 'orange']
+    errorParams = {'ecolor': 'red', 'elinewidth': 2, 'capsize': 15, 'capthick': 2}
+
+    # Display Data:
+    plt.clf()
+    plt.figure()
+    plt.bar(galTypeNames, avgStelMasses, yerr = stelMassErrors, color = barGraphColors, edgecolor = 'black', linewidth = 2, error_kw = errorParams)
+
+    plt.yscale('log')
+    plt.xlabel("Galaxy Morphological Type", fontweight = 'bold')
+    plt.ylabel("Average Stellar Median Mass (M☉)", fontweight = 'bold')
+    plt.title("Galaxy Average Stellar Median Mass and Morphological Type\n(Derived from SDSS DR18)", fontweight = 'bold')
+
+    plt.tight_layout()
+    plt.savefig("Graphs/avgStelMassandGalMorphType.png", bbox_inches= 'tight')
+
+    print(f"Average Stellar Median Mass (Spiral): {avgStelMasses[0]:.10}")
+    print(f"Average Stellar Median Mass (Elliptical): {avgStelMasses[1]:.10}")
+    print(f"T-Statistic: {tResult[0]:.10e}")
+    print(f"P-Value: {tResult[1]:.10e}")
+
+    return
+
+def analyzeAvgGalRadandGalType(galTypeNames, radSpiral, radElliptical):
+    # Spiral:
+    galRadAvgSpiral = np.mean(radSpiral)
+    galRadErrorSpiral = stdError(radSpiral)
+
+    # Elliptical:
+    galRadAvgElliptical = np.mean(radElliptical)
+    galRadErrorElliptical = stdError(radElliptical)
+
+    # Group spiral and elliptical:
+    galRadDataGrouped = np.array([radSpiral, radElliptical])
+    avgGalRads = np.array([galRadAvgSpiral, galRadAvgElliptical])
+    galRadErrors = np.array([galRadErrorSpiral, galRadErrorElliptical])
+
+    # T-Test:
+    tResult = tTest(galRadDataGrouped)
+
+    # Set up display parameters:
+    barGraphColors = ['cyan', 'orange']
+    errorParams = {'ecolor': 'red', 'elinewidth': 2, 'capsize': 15, 'capthick': 2}
+
+    # Display Data:
+    plt.clf()
+    plt.figure()
+    plt.bar(galTypeNames, avgGalRads, yerr = galRadErrors, color = barGraphColors, edgecolor = 'black', linewidth = 2, error_kw = errorParams)
+
+    plt.yscale('log')
+    plt.xlabel("Galaxy Morphological Type", fontweight = 'bold')
+    plt.ylabel("Average Galaxy Radius (arcsecond)", fontweight = 'bold')
+    plt.title("Galaxy Average Radius and Morphological Type\n(Derived from SDSS DR18)", fontweight = 'bold')
+
+    plt.tight_layout()
+    plt.savefig("Graphs/avgGalRadandGalMorphType.png", bbox_inches= 'tight')
+
+    print(f"Average Stellar Median Mass (Spiral): {avgGalRads[0]:.10}")
+    print(f"Average Stellar Median Mass (Elliptical): {avgGalRads[1]:.10}")
+    print(f"T-Statistic: {tResult[0]:.10e}")
+    print(f"P-Value: {tResult[1]:.10e}")
+
+    return
+
+def analyzeStelSurfDenandRedshift(galTypeNames, stelMassSpiral, radSpiral, zSpiral, stelMassElliptical, radElliptical, zElliptical):
+    # Spiral:
+    stelSurfDenDataSpiral = stelSurfDen(stelMassSpiral, radSpiral)
+
+    # Elliptical:
+    stelSurfDenDataElliptical = stelSurfDen(stelMassElliptical, radElliptical)
+
+    # Display Data:
+    plt.clf()
+    figStelSurfDenandZ, axStelSurfDenandZ = plt.subplots(1, 2, figsize = (15, 10), sharex = True, sharey = True)
+    axStelSurfDenandZ[0].scatter(zSpiral, stelSurfDenDataSpiral, color = 'cyan', s = 5)
+    axStelSurfDenandZ[1].scatter(zElliptical, stelSurfDenDataElliptical, color = 'orange', s = 5)
+
+    axStelSurfDenandZ[0].set_yscale('log')
+    axStelSurfDenandZ[0].set_xlabel("Galaxy Redshift (z)", fontweight = 'bold')
+    axStelSurfDenandZ[0].set_ylabel("Average Stellar Surface Density (M☉/(arcsecond^2))", fontweight = 'bold')
+    axStelSurfDenandZ[0].set_title(f"Morphological Type: {galTypeNames[0]}", fontweight = 'bold')
+
+    axStelSurfDenandZ[1].set_yscale('log')
+    axStelSurfDenandZ[1].set_xlabel("Galaxy Redshift (z)", fontweight = 'bold')
+    axStelSurfDenandZ[1].set_ylabel("Average Stellar Surface Density (M☉/(arcsecond^2))", fontweight = 'bold')
+    axStelSurfDenandZ[1].set_title(f"Morphological Type: {galTypeNames[1]}", fontweight = 'bold')
+    
+    figStelSurfDenandZ.suptitle("Galaxy Average Stellar Surface Density and Redshift\n(Derived from SDSS DR18)", fontweight = 'bold')
+
+    plt.savefig("Graphs/avgStelSurfDenandGalRedshift.png", bbox_inches= 'tight')
 
     return
 
 # Call function:
-analyzeStelSurfDenandGalType(galMorphTypeNames, spiralMedStelMass, spiralExpRadRBand, ellipticalMedStelMass, ellipticalDeVanRadRBand)
+print("Average Stellar Surface Density and Galaxy Type:")
+analyzeAvgStelSurfDenandGalType(galMorphTypeNames1, spiralMedStelMass, spiralExpRadRBand, ellipticalMedStelMass, ellipticalDeVanRadRBand)
+
+print("\nAverage Stellar Median Mass and Galaxy Type:")
+analyzeAvgMedStelMassandGalType(galMorphTypeNames1, spiralMedStelMass, ellipticalMedStelMass)
+
+print("\nAverage Galaxy Radius and Galaxy Type:")
+analyzeAvgGalRadandGalType(galMorphTypeNames1, spiralExpRadRBand, ellipticalDeVanRadRBand)
+
+print("\nStellar Surface Density and Redshift:")
+analyzeStelSurfDenandRedshift(galMorphTypeNames2, spiralMedStelMass, spiralExpRadRBand, spiralZ, ellipticalMedStelMass, ellipticalDeVanRadRBand, ellipticalZ)
